e31b01e9d92bcbb9f3cc7ff3793cfa87a25906d0
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2019-10-09 16:33:34 -0400

    Finished hw 2

--- hw2/models.py
+++ hw2/models.py
@@ -8,10 +8,16 @@ import numpy as np
 
 
 class RegressionTree(object):
-    def __init__(self, nfeatures, max_depth):
+    def __init__(self, nfeatures, max_depth, depth=0):
         self.num_input_features = nfeatures
         self.max_depth = max_depth
-        self.root = None
+        self.depth = depth
+        self.is_leaf = False
+        self.left = None
+        self.right = None
+        self.split_feature = None
+        self.threshold = None
+        self.label = None
 
     def fit(self, *, X, y):
         """ Fit the model.
@@ -20,8 +26,7 @@ class RegressionTree(object):
                 y: An array of floats with shape [num_examples].
                 max_depth: An int representing the maximum depth of the tree
         """
-        self.root = Node(max_depth=self.max_depth, depth=0)
-        self.root.build_regression_tree(X, y)
+        self.build_regression_tree(X, y)
 
     def predict(self, X):
         """ Predict.
@@ -33,26 +38,14 @@ class RegressionTree(object):
         """
         y = np.zeros(X.shape[0])
         for i in range(X.shape[0]):
-            curr = self.root
+            curr = self
             while curr.is_leaf is False:
-                if X[i, curr.split_feature] <= curr.threshold:
+                if X[i, curr.split_feature] < curr.threshold:
                     curr = curr.left
                 else:
                     curr = curr.right
             y[i] = curr.label
         return y
-
-
-class Node(RegressionTree):
-    def __init__(self, max_depth, depth):
-        self.max_depth = max_depth
-        self.depth = depth
-        self.is_leaf = False
-        self.left = None
-        self.right = None
-        self.split_feature = None
-        self.threshold = None
-        self.label = None
 
     def build_regression_tree(self, X, y):
         novar = True
@@ -65,6 +58,9 @@ class Node(RegressionTree):
             self.is_leaf = True
             self.label = y[0]
             return
+        elif np.var(y) == 0:
+            self.is_leaf = True
+            self.label = y[0]
         elif novar:
             self.is_leaf = True
             self.label = np.average(y)
@@ -75,49 +71,32 @@ class Node(RegressionTree):
             return
         else:
             left_set = self.optimized_split(X, y)
-            X_left = np.take(X, left_set, axis=0)
-            y_left = np.take(y, left_set, axis=0)
+            X_left = np.take(X, left_set, axis=0)[0]
+            y_left = np.take(y, left_set, axis=0)[0]
             X_right = np.delete(X, left_set, axis=0)
             y_right = np.delete(y, left_set, axis=0)
-            self.left = Node(self.max_depth, self.depth + 1)
-            self.right = Node(self.max_depth, self.depth + 1)
+            self.left = RegressionTree(nfeatures=X_left.shape[1], max_depth=self.max_depth, depth=self.depth + 1)
+            self.right = RegressionTree(nfeatures=X_right.shape[1], max_depth=self.max_depth, depth=self.depth + 1)
             self.left.build_regression_tree(X_left, y_left)
             self.right.build_regression_tree(X_right, y_right)
 
     def optimized_split(self, X, y):
 
-        # Perform sorting for speedup down the road
-        Xsort = np.zeros(X.shape, dtype=int)
-        for d in range(X.shape[1]):
-            Xsort[:, d] = np.argsort(X[:, d])
-
         # Calculate SSEs
         min_sse = np.inf
         for d in range(X.shape[1]):
-            visited = []
-            for i in range(X.shape[0]):
-                # Need to work backwards to ensure equal values are included properly
-                i_adj = X.shape[0]-i-1
-                threshold = X[Xsort[i_adj, d], d]
-                if visited.__contains__(threshold):
-                    continue
-                else:
-                    visited.append(threshold)
-                    left_set = Xsort[:i_adj, d]
-                    if len(left_set) == 0 or len(left_set) == len(y):
-                        continue
-                    y_left = np.take(y, left_set, axis=0)
-                    y_right = np.delete(y, left_set, axis=0)
-                    sse = sum((y_left-np.full(len(y_left), np.average(y_left))) ** 2) +\
-                        sum((y_right - np.full(len(y_right), np.average(y_right))) ** 2)
-                    if sse < min_sse:
-                        min_sse = sse
-                        self.split_feature = d
-                        self.threshold = threshold
-                        threshold_index_max = i_adj
-
-        left_set = Xsort[:threshold_index_max, self.split_feature]
-
+            splits = np.unique(X[:, d])
+            for i in range(1, len(splits)):  # split on maximum value will yield empty set
+                threshold = splits[i]
+                left = np.where(X[:, d] < np.full(X.shape[0], threshold))
+                y_left = np.take(y, left, axis=0)[0]
+                y_right = np.delete(y, left, axis=0)
+                sse = len(y_left)*np.var(y_left) + len(y_right)*np.var(y_right)
+                if sse < min_sse:
+                    min_sse = sse
+                    self.split_feature = d
+                    self.threshold = threshold
+                    left_set = left
         return left_set
 
 
@@ -128,8 +107,10 @@ class GradientBoostedRegressionTree(object):
         self.n_estimators = n_estimators
         self.regularization_parameter = regularization_parameter
         self.trees = []
+        self.f0 = 0
 
     def fit(self, *, X, y):
+        self.f0 = np.average(y)
         f = np.full(len(y), np.average(y))
         for i in range(self.n_estimators):
             self.trees.append(RegressionTree(nfeatures=self.num_input_features, max_depth=self.max_depth))
@@ -154,7 +135,7 @@ class GradientBoostedRegressionTree(object):
         Returns:
                 An array of floats with shape [num_examples].
         """
-        y = np.zeros(X.shape[0])
+        y = np.full(X.shape[0], self.f0)
         for tree in self.trees:
-            y += tree.predict(X)
+            y += self.regularization_parameter*tree.predict(X)
         return y
